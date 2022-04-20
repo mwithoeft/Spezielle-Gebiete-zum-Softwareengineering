b88b90b5c84d2f5a4c1faacbd3b56b54d87ca5d8
mwithoeft <moritz_pit.withoeft@fh-bielefeld.de>
2022-04-20 20:09:41 +0200

    feat: switch for serve websocket/webtransport in backend

--- communication-protocols/src/app/services/webtransport.service.ts
+++ communication-protocols/src/app/services/webtransport.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@angular/core';
 
+declare var WebTransport: any;
+
 @Injectable({
   providedIn: 'root'
 })
 export class WebtransportService {
 
+  readonly url = 'https://example.com:4999/foo/bar';
+
   constructor() {
     console.log("Using WebTransportService");
+    this.init();
   }
+
+  init() {
+    const wt = new WebTransport(this.url);
+  }
+
 }
