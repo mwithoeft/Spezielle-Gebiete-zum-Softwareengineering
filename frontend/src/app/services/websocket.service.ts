34b0a259d741620be60b86a5126cffdc2d979b6e
mwithoeft <moritz_pit.withoeft@fh-bielefeld.de>
2022-05-16 15:10:55 +0200

    feat: large download files with webtransport

--- frontend/src/app/services/websocket.service.ts
+++ frontend/src/app/services/websocket.service.ts
@@ -1,18 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Socket } from 'ngx-socket-io';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebsocketService {
-
   constructor(private socket: Socket) {
+  }
+
+  downloadFiles! : BehaviorSubject<string[]>;
+
+  init(downloadFiles: BehaviorSubject<string[]>) {
     console.log("Using WebSocketService");
 
+    this.downloadFiles = downloadFiles;
+
     this.socket.connect();
-    this.listenToMessage();
-    this.sendMessage("Hello from the client");
-    this.requestFiles();
+    this.activateListeners();
   }
 
 
@@ -20,17 +25,35 @@ export class WebsocketService {
     this.socket.emit('message', message);
   }
 
-  private listenToMessage() {
+  private activateListeners() {
     this.socket.on('message', (message: string) => {
       console.log("Received message via websocket: " + message);
     });
+
+    this.socket.on('download-files-list', (message: string[]) => {
+      this.downloadFiles.next(message);
+    });
+
+    this.socket.on('download-file', (filename: string, file: Uint8Array) => {
+      let blob = new Blob([file], { type: 'application/octet-stream' });
+      let url = window.URL.createObjectURL(blob);
+      let a = document.createElement('a');
+      a.href = url;
+      a.download = filename;
+      a.click();
+      console.timeEnd("downloadFile");
+    });
   }
 
-  public requestFiles() {
+  public requestAvailableFiles() {
     this.socket.emit('download-files-list');
-    this.socket.on('download-files-list', (message: string[]) => {
-      console.log(message);
-    });
+    
+
+  }
+
+  public downloadFile(filename: string) {
+    console.time("downloadFile");
+    this.socket.emit('download-file', filename);
   }
 
 }
